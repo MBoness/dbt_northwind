746d0f3fa816c1988f746907298fdc9a7edd0e09
m.bonesso@gmail.com <m.bonesso@gmail.com>
2024-06-03 01:02:39 +0000

    added first set of files

---
+++ models/marts/dim_customers.sql
@@ -0,0 +1,9 @@
+with dim_cust as (
+    select 
+    {{ dbt_utils.generate_surrogate_key(['customer_id', 'company_name']) }} as sk_customer
+    , *
+    from {{ ref("stg_customers") }}
+)
+
+select *
+from dim_cust
